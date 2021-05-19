49926e4c83649a2fe9533b92e9c909332ecf714b
Vladimir Nachbaur <vizigr0u@gmail.com>
2021-05-18 21:44:54 -0400

    add name to levels

--- src/gameConstants.js
+++ src/gameConstants.js
@@ -1,15 +1,18 @@
 export const Levels = {
     Easy: {
+        name: "Easy",
         mines: 10,
         width:9,
         height: 9
     },
     Medium: {
+        name: "Medium",
         mines: 40,
         width: 16,
         height: 16
     },
     Expert: {
+        name: "Expert",
         mines: 99,
         width: 30,
         height: 16
